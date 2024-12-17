40174f8f6ef81069932780a3969a5a5dc8b5d1e7
Bobbyloo89 <Mikael.lindstedt@live.se>
2024-12-17 15:35:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,18 @@ feedbackFormulär.addEventListener("submit", (e) => {
   feedbackFormulär.reset();
   feedbackFönster.classList.add("dold");
 });
+
+// Funktion för knapp för att stänga Gör-så-här-rutan
+const instructionsPopup = document.getElementById('instructions');
+const instructionsClose = document.getElementById('instructions-close');
+
+instructionsClose.addEventListener('click', function() {
+    instructionsPopup.style.display = 'none';
+});
+
+// Funktion för "HJÄLP"-knapp i menyraden som öppnar Gör-så-här-rutan
+// const HJÄLPKNAPP = document.getElementById('HJÄLPKNAPP');
+
+// HJÄLPKNAPP.addEventListener('click', function() {
+//     instructionsPopup.style.display = 'flex';
+// });
